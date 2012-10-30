a7861c56bfbebf6acc28db264095704c6160bbc3
Tollef Fog Heen <tfheen@varnish-software.com>
2012-10-30 13:43:53 +0100

    Add pre-build support for the workspace cleanup plugin
    Change-Id: Ib88d09a2da1c903bac9afeb1dbd70648e003edfe

--- setup.py
+++ setup.py
@@ -87,6 +87,7 @@ setup(name='jenkins_job_builder',
             'timeout=jenkins_jobs.modules.wrappers:timeout',
             'timestamps=jenkins_jobs.modules.wrappers:timestamps',
             'ansicolor=jenkins_jobs.modules.wrappers:ansicolor',
+            'workspace_cleanup=jenkins_jobs.modules.wrappers:workspace_cleanup',
             ],
         'jenkins_jobs.modules': [
             'assignednode=jenkins_jobs.modules.assignednode:AssignedNode',
