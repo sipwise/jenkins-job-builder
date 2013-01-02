bb35082e595c9bc7acdda140c235af22c69e09a6
Will Soula <william.soula@drillinginfo.com>
2013-01-02 11:23:42 -0600

    Fixing Inject to not create empty tags
    Using the Inject functionality with the builders section caused
    an error in Jenkins because the script-file was blank but the tag
    existed in the xml.  Fixing this by only putting in the tag if
    the value exists in the yaml.  This changes the inject functionality
    in builders.py properties.by and wrappers.py.  It checks for the
    following properties to exist before creating the tag: properties-file
    properties-content script-file script-content and groovy-content
    
    Change-Id: I9151f7444f6807e929721b04fb8e7ad3a867cd6c

--- jenkins_jobs/modules/builders.py
+++ jenkins_jobs/modules/builders.py
@@ -1,4 +1,4 @@
-# Copyright 2012 Hewlett-Packard Development Company, L.P.
+# opyright 2012 Hewlett-Packard Development Company, L.P.
 # Copyright 2012 Varnish Software AS
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -271,10 +271,10 @@ def inject(parser, xml_parent, data):
     """
     eib = XML.SubElement(xml_parent, 'EnvInjectBuilder')
     info = XML.SubElement(eib, 'info')
-    propfile = data.get('properties-file', '')
-    XML.SubElement(info, 'propertiesFilePath').text = propfile
-    propcontent = data.get('properties-content', '')
-    XML.SubElement(info, 'propertiesContent').text = propcontent
+    jenkins_jobs.modules.base.add_nonblank_xml_subelement(
+        'properties-file', 'propertiesFilePath', info, data)
+    jenkins_jobs.modules.base.add_nonblank_xml_subelement(
+        'properties-content', 'propertiesContent', info, data)
 
 
 def artifact_resolver(parser, xml_parent, data):
