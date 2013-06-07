db21cf72aa5b2c13f090cdc25208bce0102c4d48
Darragh Bailey <dbailey@hp.com>
2013-06-07 05:04:32 +0100

    Use yaml local tags to support including files
    Add support for local tags which are application specific to allow
    including of other yaml files or code from scripts. Allows for code to
    be maintained and tested as seperate files, as well as reduces
    duplication of yaml code that cannot be macro'ed or easily templated by
    including it from a common file.
    
    Adds support for the following tags:
      'include' - load file as yaml code
      'include-raw' - load file as data (for scripts)
      'include-raw-escaped' - load file as data with escaping braces '{}'
          as default for use with job-templates
    
    Use configuration file options to provide a search path for the files.
    
    - Test behaviour of yaml tags independent of any XML generation
      by comparing json result of yaml parsing to verify that certain
      tags do/don't recall the yaml.load() method.
    - Add examples for the include tags via addition tests for YamlParser
      class
    
    Inspired by
    http://stackoverflow.com/questions/528281/how-can-i-include-an-yaml-file-inside-another
    
    Change-Id: Ib90a07043112d4739d6529ceddbc9817668bcec0

--- jenkins_jobs/builder.py
+++ jenkins_jobs/builder.py
@@ -30,6 +30,8 @@ import copy
 import itertools
 import fnmatch
 from jenkins_jobs.errors import JenkinsJobsException
+import local_yaml
+import ConfigParser
 
 logger = logging.getLogger(__name__)
 MAGIC_MANAGE_STRING = "<!-- Managed by Jenkins Job Builder -->"
@@ -121,9 +123,16 @@ class YamlParser(object):
         self.jobs = []
         self.config = config
         self.registry = ModuleRegistry(self.config)
+        self.path = ["."]
+        if self.config:
+            try:
+                self.path = config.get('yaml_loader',
+                                       'include_path').split(':')
+            except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
+                pass
 
     def parse_fp(self, fp):
-        data = yaml.load(fp)
+        data = local_yaml.load(fp, search_path=self.path)
         if data:
             if not isinstance(data, list):
                 raise JenkinsJobsException(
