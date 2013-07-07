df6568ab059435a416ace2c01bc444aba80bc2ae
Arnaud Fabre <fabre.arnaud@gmail.com>
2013-07-07 22:23:58 +0200

    RFC: Allow defaults to inherit each other.
    When managing a lot of different jobs, distinct defaults are useful,
    but they often share a small amount of configuration.  Currently,
    there is no other way to handle this than to duplicate that
    configuration between defaults.
    
    This patch aims at improving the situation, with a simple inheritance
    system between defaults.  A defaults can declare that it wants to
    inherit from another set of defaults by having a 'inherit: [defaults-name]'
    in its definition.
    
    Change-Id: Id434915194c6d9feb19de452e892bd9f2ed2c072

--- jenkins_jobs/builder.py
+++ jenkins_jobs/builder.py
@@ -93,6 +93,22 @@ class YamlParser(object):
             return job
         return self.applyDefaults(job)
 
+    def resolveDefaults(self):
+        defaults = self.data.get('defaults', {})
+        defaults_to_resolve = [d for d in defaults.values() if 'inherit' in d]
+        while defaults_to_resolve:
+            # Find defaults whose parent are already fully resolved.
+            roots = [d for d in defaults_to_resolve
+                     if 'inherit' not in defaults[d['inherit']]]
+            if not roots:
+                raise JenkinsJobsException("Cycle detected in defaults"
+                                           "definition")
+            for default_to_resolve in roots:
+                default_to_resolve.update(
+                    defaults[default_to_resolve['inherit']])
+                del default_to_resolve['inherit']
+                defaults_to_resolve.remove(default_to_resolve)
+
     def applyDefaults(self, data):
         whichdefaults = data.get('defaults', 'global')
         defaults = self.data.get('defaults', {}).get(whichdefaults, {})
@@ -419,6 +435,7 @@ class Builder(object):
         for in_file in files_to_process:
             logger.debug("Parsing YAML file {0}".format(in_file))
             parser.parse(in_file)
+        parser.resolveDefaults()
         if names:
             logger.debug("Will filter out jobs not in %s" % names)
         parser.generateXML(names)
