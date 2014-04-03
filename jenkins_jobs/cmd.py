dab66f3085ad4331cbd952c24e7fcda2819086a1
David Caro <dcaroest@redhat.com>
2014-04-03 18:42:31 +0200

    Added recursive option
    Now you can organize your yaml files into subdirectories and apply them
    all just passing --recursive option or adding the proper configuration
    file option
    
    Change-Id: I126751e347622716c592c6ed1a57b8acb7bf79a4

--- jenkins_jobs/cmd.py
+++ jenkins_jobs/cmd.py
@@ -26,6 +26,7 @@ DEFAULT_CONF = """
 [job_builder]
 keep_descriptions=False
 ignore_cache=False
+recursive=False
 
 [jenkins]
 url=http://localhost:8080/
@@ -40,19 +41,32 @@ def confirm(question):
         sys.exit('Aborted')
 
 
+def recurse_path(root):
+    basepath = os.path.realpath(root)
+    pathlist = [basepath]
+
+    for root, dirs, files in os.walk(basepath, topdown=True):
+        pathlist.extend([os.path.join(root, path) for path in dirs])
+
+    return pathlist
+
+
 def main():
     import jenkins_jobs.builder
     import jenkins_jobs.errors
     parser = argparse.ArgumentParser()
+    recursive_parser = argparse.ArgumentParser(add_help=False)
+    recursive_parser.add_argument('-r', '--recursive', action='store_true',
+                                  help='look for yaml files recusrsively')
     subparser = parser.add_subparsers(help='update, test or delete job',
                                       dest='command')
-    parser_update = subparser.add_parser('update')
+    parser_update = subparser.add_parser('update', parents=[recursive_parser])
     parser_update.add_argument('path', help='path to YAML file or directory')
     parser_update.add_argument('names', help='name(s) of job(s)', nargs='*')
     parser_update.add_argument('--delete-old', help='delete obsolete jobs',
                                action='store_true',
                                dest='delete_old', default=False,)
-    parser_test = subparser.add_parser('test')
+    parser_test = subparser.add_parser('test', parents=[recursive_parser])
     parser_test.add_argument('path', help='path to YAML file or directory',
                              nargs='?', default=sys.stdin)
     parser_test.add_argument('-o', dest='output_dir', default=sys.stdout,
@@ -147,6 +161,12 @@ def main():
             logger.warn(
                 "Reading configuration from STDIN. Press %s to end input.",
                 key)
+    else:
+        if (hasattr(options, 'recursive') or
+            config.has_option('job_builder', 'recursive')) and \
+                os.path.isdir(options.path):
+
+            options.path = recurse_path(options.path)
 
     if options.command == 'delete':
         for job in options.name:
