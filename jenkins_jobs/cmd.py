593dea1dc677644232373437822bdb3beb29884c
Marc Abramowitz <marc@marc-abramowitz.com>
2014-04-05 23:13:11 -0700

    Add `browse` command
    which opens Jenkins web page(s) for job(s) in browser
    
    Change-Id: I135a58b2fb270707d8db5d65fc2e82c90bc5d940

--- jenkins_jobs/cmd.py
+++ jenkins_jobs/cmd.py
@@ -68,6 +68,14 @@ def main(argv=None):
     parser_delete.add_argument('name', help='name of job', nargs='+')
     parser_delete.add_argument('-p', '--path', default=None,
                                help='path to YAML file or directory')
+    parser_browse = subparser.add_parser('browse')
+    parser_browse.add_argument('names', help='name(s) of job(s)', nargs='*',
+                               default=None)
+    parser_browse.add_argument('-b', '--batch', default=10,
+                               help='number of browser windows to open '
+                                    'at once')
+    parser_browse.add_argument('-p', '--path', default='.',
+                               help='Path to YAML file or directory')
     subparser.add_parser('delete-all',
                          help='delete *ALL* jobs from Jenkins server, '
                          'including those not managed by Jenkins Job '
@@ -158,6 +166,11 @@ def main(argv=None):
         for job in options.name:
             logger.info("Deleting jobs in [{0}]".format(job))
             builder.delete_job(job, options.path)
+    elif options.command == 'browse':
+        logger.info("Browsing jobs in {0} ({1})".format(
+            options.path, options.names))
+        builder.browse_jobs(options.names, options.path,
+                            batch=int(options.batch))
     elif options.command == 'delete-all':
         confirm('Sure you want to delete *ALL* jobs from Jenkins server?\n'
                 '(including those not managed by Jenkins Job Builder)')
