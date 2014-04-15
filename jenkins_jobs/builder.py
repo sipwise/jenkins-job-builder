21953d312bc9e251721156c9e31953399df71d8c
Marc Abramowitz <marc@marc-abramowitz.com>
2014-04-15 16:11:01 -0700

    Some tweaks to get closer to Python 3 compat
    Change-Id: If4b35e2ceee8239379700e22eb79a3eaa04d6f0f

--- jenkins_jobs/builder.py
+++ jenkins_jobs/builder.py
@@ -16,6 +16,7 @@
 # Manage jobs in Jenkins server
 
 import os
+import operator
 import sys
 import hashlib
 import yaml
@@ -130,7 +131,7 @@ class YamlParser(object):
                     " not a {cls}".format(fname=getattr(fp, 'name', fp),
                                           cls=type(data)))
             for item in data:
-                cls, dfn = item.items()[0]
+                cls, dfn = list(item.items())[0]
                 group = self.data.get(cls, {})
                 if len(item.items()) > 1:
                     n = None
@@ -269,6 +270,7 @@ class YamlParser(object):
             # us guarantee a group of parameters will not be added a
             # second time.
             uniq = json.dumps(expanded, sort_keys=True)
+            uniq = uniq.encode('utf-8')
             checksum = hashlib.md5(uniq).hexdigest()
 
             # Lookup the checksum
@@ -325,7 +327,7 @@ class ModuleRegistry(object):
             Mod = entrypoint.load()
             mod = Mod(self)
             self.modules.append(mod)
-            self.modules.sort(lambda a, b: cmp(a.sequence, b.sequence))
+            self.modules.sort(key=operator.attrgetter('sequence'))
             if mod.component_type is not None:
                 self.modules_by_component_type[mod.component_type] = mod
 
@@ -368,8 +370,8 @@ class ModuleRegistry(object):
             .component_list_type
 
         if isinstance(component, dict):
-            # The component is a sigleton dictionary of name: dict(args)
-            name, component_data = component.items()[0]
+            # The component is a singleton dictionary of name: dict(args)
+            name, component_data = component.items().pop(0)
             if template_data:
                 # Template data contains values that should be interpolated
                 # into the component definition
--- jenkins_jobs/modules/hipchat_notif.py
+++ jenkins_jobs/modules/hipchat_notif.py
@@ -46,7 +46,7 @@ import xml.etree.ElementTree as XML
 import jenkins_jobs.modules.base
 import jenkins_jobs.errors
 import logging
-import ConfigParser
+from jenkins_jobs.compat import ConfigParser
 import sys
 
 logger = logging.getLogger(__name__)
@@ -74,7 +74,7 @@ class HipChat(jenkins_jobs.modules.base.Base):
                     raise jenkins_jobs.errors.JenkinsJobsException(
                         "Hipchat authtoken must not be a blank string")
             except (ConfigParser.NoSectionError,
-                    jenkins_jobs.errors.JenkinsJobsException), e:
+                    jenkins_jobs.errors.JenkinsJobsException) as e:
                 logger.fatal("The configuration file needs a hipchat section" +
                              " containing authtoken:\n{0}".format(e))
                 sys.exit(1)
