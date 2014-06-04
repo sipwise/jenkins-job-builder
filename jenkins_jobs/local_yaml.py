a1597053027a874d477148100843ea844a4a9a4a
Marc Abramowitz <marc@marc-abramowitz.com>
2014-06-04 10:35:13 -0700

    Some tweaks to get closer to Python 3 compat
    Includes https://gist.github.com/enaeseth/844388 which ensures data read
    by yaml will have it's order retained in a predictable manner across
    multiple python versions.
    
    Change-Id: If4b35e2ceee8239379700e22eb79a3eaa04d6f0f

--- jenkins_jobs/local_yaml.py
+++ jenkins_jobs/local_yaml.py
@@ -53,6 +53,10 @@ Example:
 
 """
 
+try:
+    from collections import OrderedDict
+except ImportError:
+    from ordereddict import OrderedDict
 import functools
 import logging
 import re
@@ -115,7 +119,12 @@ class LocalLoader(yaml.Loader):
         self.add_constructor('!include-raw-escape',
                              self._include_raw_escape_tag)
 
-        if isinstance(self.stream, file):
+        # constructor to preserve order of maps and ensure that the order of
+        # keys returned is consistent across multiple python versions
+        self.add_constructor(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+                             type(self).construct_yaml_map)
+
+        if hasattr(self.stream, 'name'):
             self.search_path.add(os.path.normpath(
                 os.path.dirname(self.stream.name)))
         self.search_path.add(os.path.normpath(os.path.curdir))
@@ -153,6 +162,32 @@ class LocalLoader(yaml.Loader):
 
     def _escape(self, data):
         return re.sub(r'({|})', r'\1\1', data)
+
+    def construct_yaml_map(self, node):
+        data = OrderedDict()
+        yield data
+        value = self.construct_mapping(node)
+
+        if isinstance(node, yaml.MappingNode):
+            self.flatten_mapping(node)
+        else:
+            raise yaml.constructor.ConstructorError(
+                None, None,
+                'expected a mapping node, but found %s' % node.id,
+                node.start_mark)
+
+        mapping = OrderedDict()
+        for key_node, value_node in node.value:
+            key = self.construct_object(key_node, deep=False)
+            try:
+                hash(key)
+            except TypeError as exc:
+                raise yaml.constructor.ConstructorError(
+                    'while constructing a mapping', node.start_mark,
+                    'found unacceptable key (%s)' % exc, key_node.start_mark)
+            value = self.construct_object(value_node, deep=False)
+            mapping[key] = value
+        data.update(mapping)
 
 
 def load(stream, **kwargs):
