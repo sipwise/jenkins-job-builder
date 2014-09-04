743031b0285611f543bb95c2bf519a7e0269e11f
aidan.mcginley <aidan.mcginley@wonga.com>
2014-09-04 11:04:03 +0100

    Adds support for the Config File Provider Plugin
    See https://wiki.jenkins-ci.org/display/JENKINS/Config+File+Provider+Plugin
    
    Change-Id: Ie61bc6c4f934baf7e96bf943efcd66b138ef8edd

--- jenkins_jobs/modules/wrappers.py
+++ jenkins_jobs/modules/wrappers.py
@@ -57,6 +57,39 @@ def ci_skip(parser, xml_parent, data):
     })
 
 
+def config_file_provider(parser, xml_parent, data):
+    """yaml: config-file-provider
+    Provide configuration files (i.e., settings.xml for maven etc.)
+    which will be copied to the job's workspace.
+    Requires the Jenkins `Config File Provider Plugin.
+    <https://wiki.jenkins-ci.org/display/JENKINS/Config+File+Provider+Plugin>`_
+
+    :arg str file-id: Set the new version number for replace (default 1.0.0)
+    :arg str target: Optionally defines where the file should be created
+    :arg str variable: Optionally Defines an environment variable to be
+
+    Example:
+
+    .. literalinclude:: \
+    /../../tests/builders/fixtures/config-file-provider001.yaml
+       :language: yaml
+    """
+    top = XML.SubElement(xml_parent, 'org.jenkinsci.plugins.configfiles.'
+                         'buildwrapper.ConfigFileBuildWrapper')
+    xml_files = XML.SubElement(top, 'managedFiles')
+
+    files = data.get('files', [])
+    for file in files:
+        xml_file = XML.SubElement(xml_files, 'org.jenkinsci.plugins.'
+                                  'configfiles.buildwrapper.ManagedFile')
+        XML.SubElement(xml_file, 'fileId').text = \
+            file['file'].get('file-id')
+        XML.SubElement(xml_file, 'targetLocation').text = \
+            file['file'].get('target', '')
+        XML.SubElement(xml_file, 'variable').text = \
+            file['file'].get('variable', '')
+
+
 def logfilesize(parser, xml_parent, data):
     """yaml: logfilesize
     Abort the build if its logfile becomes too big.
