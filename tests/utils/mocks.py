4d39a2ab91d4feb84265a4af7a04b29732bc3b14
Wayne <wayne@puppetlabs.com>
2014-11-11 23:50:45 -0800

    Reorganize tests/cmd
    This commit reorganizes the 'cmd' tests by subcommand and adds a utils
    directory with a mocks module that contains reusable content intended
    to support mocking os.walk as well as to provide a consistent set of
    paths for use during mutlipath testing.
    
    Previously the multipath test was using an actual fixture directory
    which actually made the fixtures directory somewhat unusable by other
    tests since with an upcoming change (get_plugins_version) fixtures
    directories may also contain YAML that doesn't contain JJB data.
    
    Change-Id: I4c3866e1e0769512b40e1790e40affedb13953a2

---
+++ tests/utils/mocks.py
@@ -0,0 +1,32 @@
+# These are mock helpers intended to help stub standard library functions.
+
+os_walk_return_values = {
+    '/jjb_projects': [
+        ('/jjb_projects', ('dir1', 'dir2', 'dir3'), ()),
+        ('/jjb_projects/dir1', ('bar',), ()),
+        ('/jjb_projects/dir2', ('baz',), ()),
+        ('/jjb_projects/dir3', (), ()),
+        ('/jjb_projects/dir1/bar', (), ()),
+        ('/jjb_projects/dir2/baz', (), ()),
+    ],
+    '/jjb_templates': [
+        ('/jjb_templates', ('dir1', 'dir2', 'dir3'), ()),
+        ('/jjb_templates/dir1', ('bar',), ()),
+        ('/jjb_templates/dir2', ('baz',), ()),
+        ('/jjb_templates/dir3', (), ()),
+        ('/jjb_templates/dir1/bar', (), ()),
+        ('/jjb_templates/dir2/baz', (), ()),
+    ],
+    '/jjb_macros': [
+        ('/jjb_macros', ('dir1', 'dir2', 'dir3'), ()),
+        ('/jjb_macros/dir1', ('bar',), ()),
+        ('/jjb_macros/dir2', ('baz',), ()),
+        ('/jjb_macros/dir3', (), ()),
+        ('/jjb_macros/dir1/bar', (), ()),
+        ('/jjb_macros/dir2/baz', (), ()),
+    ],
+}
+
+
+def os_walk_side_effects(path_name, topdown):
+    return os_walk_return_values[path_name]
