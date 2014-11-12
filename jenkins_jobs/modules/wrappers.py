ca538c19f25eb596e22786476aad036e1b7048b2
Kyle Rockman <kyle.rockman@mac.com>
2014-11-12 10:46:11 -0600

    adding support for custom tools plugin
    Change-Id: I578a7dcee3b6ace123c0d13b8a9ae51d94a21fcd
    Signed-off-by: Kyle Rockman <kyle.rockman@mac.com>

--- jenkins_jobs/modules/wrappers.py
+++ jenkins_jobs/modules/wrappers.py
@@ -1173,6 +1173,27 @@ def credentials_binding(parser, xml_parent, data):
             credential_xml.text = params.get('credential-id')
 
 
+def custom_tools(parser, xml_parent, data):
+    """yaml: custom-tools
+    Requires the Jenkins Custom Tools Plugin.
+    <https://wiki.jenkins-ci.org/display/JENKINS/Custom+Tools+Plugin>`_
+
+    Example:
+
+    .. literalinclude:: /../../tests/wrappers/fixtures/custom-tools001.yaml
+    """
+    wrapper = XML.SubElement(xml_parent,
+                             'com.cloudbees.jenkins.plugins.customtools.CustomToolInstallWrapper')
+    XML.SubElement(wrapper,
+                   'convertHomesToUppercase').text = str(data.get('convert-homes-to-upper', 'false'))
+    
+    wrapper_tools = XML.SubElement(wrapper, 'selectedTools')
+    tools = data.get('tools', [])
+    for tool in tools:
+        wrapper = XML.SubElement(wrapper_tools,
+                                 'com.cloudbees.jenkins.plugins.customtools.CustomToolInstallWrapper_-SelectedTool')
+        XML.SubElement(wrapper, 'name').text = str(tool)
+        
 class Wrappers(jenkins_jobs.modules.base.Base):
     sequence = 80
 
