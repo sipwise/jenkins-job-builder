5e0289b0146894a41cf5d64e82caeb4766db68cf
Kei YAMAZAKI <daydream.yamazaki@gmail.com>
2014-12-04 20:39:46 +0900

    Add retry option to multijob
    Change-Id: I0063380a79f9a66cd4dda02931428ad711f5b02f

--- jenkins_jobs/modules/builders.py
+++ jenkins_jobs/modules/builders.py
@@ -1065,6 +1065,8 @@ def multijob(parser, xml_parent, data):
                 * **kill-phase-on** (`str`) -- Stop the phase execution
                   on specific job status. Can be 'FAILURE', 'UNSTABLE',
                   'NEVER'. (optional)
+                * **retry** (`int`) -- The maximum retry for this build.
+                * **parsing-rules-path** (`str`) -- Parsing rules file path
 
     Example:
 
@@ -1091,6 +1093,14 @@ def multijob(parser, xml_parent, data):
         # Pass through the current build params
         currParams = str(project.get('current-parameters', False)).lower()
         XML.SubElement(phaseJob, 'currParams').text = currParams
+
+        # Retry job
+        retry = project.get('retry')
+        if retry is not None:
+            rules_path = project.get('parsing-rules-path')
+            XML.SubElement(phaseJob, 'parsingRulesPath').text = rules_path
+            XML.SubElement(phaseJob, 'maxRetries').text = str(retry)
+            XML.SubElement(phaseJob, 'enableRetryStrategy').text = "true"
 
         # Pass through other params
         configs = XML.SubElement(phaseJob, 'configs')
