ecfba0a2fee02e34f0fbb1a979fcee4dd239ad1f
Jan Hruban <jan.hruban@gooddata.com>
2015-03-23 07:35:58 +0100

    Add support for Display Upstream Changes Plugin
    Supports publisher for displaying SCM changes for upstream jobs
    https://wiki.jenkins-ci.org/display/JENKINS/Display+Upstream+Changes+Plugin
    
    Change-Id: I7720220ce05e2882e407ea75ca0c689cbd8c9a0b

--- jenkins_jobs/modules/publishers.py
+++ jenkins_jobs/modules/publishers.py
@@ -3985,6 +3985,23 @@ def conditional_publisher(parser, xml_parent, data):
             raise JenkinsJobsException('action must be set for each condition')
 
 
+def display_upstream_changes(parser, xml_parent, data):
+    """yaml: display-upstream-changes
+    Display SCM changes of upstream jobs. Requires the Jenkins
+    :jenkins-wiki:`Display Upstream Changes Plugin
+    <Display+Upstream+Changes+Plugin>`.
+
+    Example:
+
+    .. literalinclude:: \
+    /../../tests/publishers/fixtures/display-upstream-changes.yaml
+    """
+    XML.SubElement(
+        xml_parent,
+        'jenkins.plugins.displayupstreamchanges.'
+        'DisplayUpstreamChangesRecorder')
+
+
 class Publishers(jenkins_jobs.modules.base.Base):
     sequence = 70
 
