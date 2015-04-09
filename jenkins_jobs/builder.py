b03c5095a27f673c7040684b3621a95ad37c237c
Victor Stinner <vstinner@redhat.com>
2015-04-10 00:52:28 +0200

    Fix Builder.update_job() on Python 3
    * update_job() now opens output file in binary mode because job.output()
      is a bytes string.
    * update_job() expects a text file for output parameter, so decode the
      XML output before calling write().
    * Fix unit test: sys.stdout is unicode on Python 3, no need to decode
      its value when sys.stdout is mocked with StringIO.
    * Enable test_builder.py tests (tests/builder/__init__.py was missing).
    * Add unit tests for Builder.update_job()
    * Add py34 to environments in tox.ini
    
    Change-Id: Id1c032c92755b0986457ec3e12cd47361e7fb7de

--- jenkins_jobs/builder.py
+++ jenkins_jobs/builder.py
@@ -28,6 +28,7 @@ import jenkins
 import re
 from pprint import pformat
 import logging
+import six
 
 from jenkins_jobs.constants import MAGIC_MANAGE_STRING
 from jenkins_jobs.parser import YamlParser
@@ -318,8 +319,12 @@ class Builder(object):
                     # `output` is a file-like object
                     logger.info("Job name:  %s", job.name)
                     logger.debug("Writing XML to '{0}'".format(output))
+                    xml = job.output()
+                    if six.PY3:
+                        # output is a text file, write() expects unicode
+                        xml = xml.decode('utf-8')
                     try:
-                        output.write(job.output())
+                        output.write(xml)
                     except IOError as exc:
                         if exc.errno == errno.EPIPE:
                             # EPIPE could happen if piping output to something
@@ -331,9 +336,8 @@ class Builder(object):
 
                 output_fn = os.path.join(output, job.name)
                 logger.debug("Writing XML to '{0}'".format(output_fn))
-                f = open(output_fn, 'w')
-                f.write(job.output())
-                f.close()
+                with open(output_fn, 'wb') as f:
+                    f.write(job.output())
                 continue
             md5 = job.md5()
             if (self.jenkins.is_job(job.name)
