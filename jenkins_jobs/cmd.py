9193379ff566d19ef31b442ee4b882eec03aab04
Max Rydahl Andersen <manderse@redhat.com>
2015-04-29 20:30:27 +0200

    Add option to skip certification check
    adds a `no_check_certificate` to the [jenkins] section.
    
    If set to True https/ssl connections will skip certificate check,
    allowing you to connect to systems that might have an outdated or
    non-public certificate. Default is and should always be False.
    
    Supported only by Python 2.7.9+ below or higher (3.0) it will be ignored.
    
    Change-Id: Ieca1c07f5142b7ac379215bfed35ca8c9d8b771e

--- jenkins_jobs/cmd.py
+++ jenkins_jobs/cmd.py
@@ -43,6 +43,7 @@ url=http://localhost:8080/
 user=
 password=
 query_plugins_info=True
+no_check_certificate=True
 
 [hipchat]
 authtoken=dummy
@@ -185,6 +186,7 @@ def setup_config_settings(options):
         if os.path.isfile(localconf):
             conf = localconf
     config = configparser.ConfigParser()
+
     # Load default config always
     config.readfp(StringIO(DEFAULT_CONF))
     if os.path.isfile(conf):
@@ -216,6 +218,27 @@ def execute(options, config):
         ignore_cache = config.getboolean('jenkins', 'ignore_cache')
     elif config.has_option('job_builder', 'ignore_cache'):
         ignore_cache = config.getboolean('job_builder', 'ignore_cache')
+
+    nocertcheck = False
+    try:
+        nocertcheck = config.get('jenkins', 'no_check_certificate')
+    except (TypeError, configparser.NoOptionError):
+        nocertcheck = True
+
+    if nocertcheck:
+        if sys.version_info >= (2, 7, 9) or
+            sys.version_info <= (3, 0, 0):
+            logger.warn('Certificate check explicitly disabled'
+                        ' in [jenkins] section.'
+                        ' Communication with jenkins server is'
+                        ' not certified.')
+            import ssl
+            ssl._create_default_https_context = ssl._create_unverified_context
+        else:
+            logger.warn('Certificate check explicitly disabled'
+                        ' in [jenkins] section,'
+                        ' but not running Python 2.7.9.'
+                        ' Running with default certificate check')
 
     # workaround for python 2.6 interpolation error
     # https://bugs.launchpad.net/openstack-ci/+bug/1259631
