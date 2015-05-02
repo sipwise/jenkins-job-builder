7500cbaeadb12b0eb2b2c63ae2897301e4d976c8
Dawid Malinowski <dawidmalina@gmail.com>
2015-05-02 19:22:46 +0200

    Add support for OWASP Dependency-Check Plugin
    Supports publisher for Dependency-Check utility that identifies project
    dependencies and checks if there are any known, publicly disclosed,
    vulnerabilities.
    https://wiki.jenkins-ci.org/display/JENKINS/OWASP+Dependency-Check+Plugin
    
    Change-Id: I3dc1ab923c392aac00189c3f852a1138c1f0ab36

--- jenkins_jobs/modules/helpers.py
+++ jenkins_jobs/modules/helpers.py
@@ -76,6 +76,11 @@ def build_trends_publisher(plugin_name, xml_element, data):
         elif key == 'health-threshold' and config_value not in thresholds:
             raise JenkinsJobsException("health-threshold must be one of %s" %
                                        ", ".join(thresholds))
+        elif key == 'use-stable-build-as-reference' and '[DependencyCheck]' in plugin_name:
+            XML.SubElement(xml_element, 'usePreviousBuildAsReference') \
+                .text = \
+                str(data.get('use-previous-build-as-reference', False)).lower()
+            xml_config.text = str(config_value).lower()
         else:
             if isinstance(default, bool):
                 xml_config.text = str(config_value).lower()
