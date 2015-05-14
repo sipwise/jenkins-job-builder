e2e8f82423855468e3334eb608841554b6835476
Richard Pijnenburg <richard.pijnenburg@gmail.com>
2015-05-14 15:49:58 +0200

    Add logstash reporter
    The logstash reporter sends the build log to Logstash via Redis or
    RabbitMQ
    
    Change-Id: I85bfc9c4a70cd4ced79bdb29f95ee6b514b72e4f

--- jenkins_jobs/modules/reporters.py
+++ jenkins_jobs/modules/reporters.py
@@ -137,6 +137,30 @@ def findbugs(parser, xml_parent, data):
     build_trends_publisher('[FINDBUGS] ', findbugs, data)
 
 
+def logstash(parser, xml_parent, data):
+    """yaml: logstash
+    Send logs to logstash
+
+    :arg num max-lines: The maximum number of log lines to send to Logstash.
+    :arg bool fail-build: Mark build as failed if this step fails.
+
+    Example::
+
+      reporters:
+        - logstash:
+            max-lines: 1000
+            fail-build: false
+    """
+
+    logstash = XML.SubElement(xml_parent,
+                              'jenkins.plugins.logstash.LogstashNotifier')
+    XML.SubElement(logstash, 'maxLines').text = str(
+        data.get('max-lines', 1000))
+
+    XML.SubElement(logstash, 'failBuild').text = str(
+        data.get('fail-build', False))
+
+
 class Reporters(jenkins_jobs.modules.base.Base):
     sequence = 55
 
