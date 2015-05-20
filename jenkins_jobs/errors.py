d8df3741ae0eea968df5335df481a653f801c0f6
Jaroslav Henner <jhenner@redhat.com>
2015-05-20 18:44:53 +0200

    Refactor err reporting of incorrect YAML structure
    This improves the error reporting of incorrect structure of the YAML -- when
    the missing indent is detected. It adds the information about the file
    name with problematic structure.
    
    Code which is extracting the expected single item from a collection has
    been extracted to `get_the_only_one_subitem` function to increase the
    readability.
    
    Added tests for missing indent exception.
    
    Change-Id: Ieebc778416998f3c3427f957c942391f2f5475dd

--- jenkins_jobs/errors.py
+++ jenkins_jobs/errors.py
@@ -65,5 +65,37 @@ class MissingAttributeError(ModuleError):
         super(MissingAttributeError, self).__init__(message)
 
 
-class YAMLFormatError(JenkinsJobsException):
-    pass
+class YAMLStructureError(JenkinsJobsException):
+    """Raised when the structure of YAML is not as expected."""
+
+    def __init__(self, *args, **kwargs):
+        super(YAMLStructureError, self).__init__(*args, **kwargs)
+        self._item_name = self._near = self._file_name = None
+
+    def item_name(self, name):
+        """Specifies the name of the problematic item."""
+        self._item_name = name
+        return self
+
+    def near(self, item):
+        """Specifies the definition of the problematic item."""
+        self._near = item
+        return self
+
+    def file_name(self, fname):
+        """Specifies the name of file where the problematic item has been
+        found."""
+        self._file_name = fname
+        return self
+
+    def __str__(self):
+        msg = []
+        if self.message:
+            msg.append(self.message)
+        if self._item_name:
+            msg.append("Item named: '{name}'".format(name=self._item_name))
+        if self._file_name:
+            msg.append("In file: {file}".format(file=self._file_name))
+        if self._near:
+            msg.append("Near: {near}".format(near=self._near))
+        return '\n  '.join(msg)
