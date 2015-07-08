74864c9019777895f0382a6d2ccc72931f7ef255
Arnold Bechtoldt <mail@arnoldbechtoldt.com>
2015-07-08 14:46:13 +0200

    support com.cloudbees.plugins.credentials properties in authorization matrix
    Change-Id: I8712a367122f6fadcaf62d4b4f144beab981bed8

--- jenkins_jobs/modules/properties.py
+++ jenkins_jobs/modules/properties.py
@@ -278,6 +278,11 @@ def authorization(parser, xml_parent, data):
     Specifies an authorization matrix
 
     The available rights are:
+      credentials-create
+      credentials-update
+      credentials-delete
+      credentials-view
+      credentials-manage-domains
       job-delete
       job-configure
       job-read
@@ -312,6 +317,16 @@ def authorization(parser, xml_parent, data):
     """
 
     mapping = {
+        'credentials-create':
+        'com.cloudbees.plugins.credentials.CredentialsProvider.Create',
+        'credentials-update':
+        'com.cloudbees.plugins.credentials.CredentialsProvider.Update',
+        'credentials-delete':
+        'com.cloudbees.plugins.credentials.CredentialsProvider.Delete',
+        'credentials-view':
+        'com.cloudbees.plugins.credentials.CredentialsProvider.View',
+        'credentials-manage-domains':
+        'com.cloudbees.plugins.credentials.CredentialsProvider.ManageDomains',
         'job-delete': 'hudson.model.Item.Delete',
         'job-configure': 'hudson.model.Item.Configure',
         'job-read': 'hudson.model.Item.Read',
@@ -322,7 +337,7 @@ def authorization(parser, xml_parent, data):
         'job-cancel': 'hudson.model.Item.Cancel',
         'run-delete': 'hudson.model.Run.Delete',
         'run-update': 'hudson.model.Run.Update',
-        'scm-tag': 'hudson.scm.SCM.Tag'
+        'scm-tag': 'hudson.scm.SCM.Tag',
     }
 
     if data:
