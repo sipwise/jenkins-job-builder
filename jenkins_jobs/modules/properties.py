a47331e548ee7ce8a396718e45d2eaa7f8b012fb
Arnold Bechtoldt <mail@arnoldbechtoldt.com>
2015-07-08 14:46:13 +0200

    Add support for credentials properties in authorization matrix
    Change-Id: I8712a367122f6fadcaf62d4b4f144beab981bed8
    Co-Authored-By: Thanh Ha <thanh.ha@linuxfoundation.org>
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jenkins_jobs/modules/properties.py
+++ jenkins_jobs/modules/properties.py
@@ -305,6 +305,11 @@ def authorization(parser, xml_parent, data):
     Specifies an authorization matrix
 
     The available rights are:
+      credentials-create
+      credentials-update
+      credentials-delete
+      credentials-view
+      credentials-manage-domains
       job-delete
       job-configure
       job-read
@@ -317,28 +322,23 @@ def authorization(parser, xml_parent, data):
       run-update
       scm-tag
 
-    Example::
+    Example:
 
-      properties:
-        - authorization:
-            admin:
-              - job-delete
-              - job-configure
-              - job-read
-              - job-discover
-              - job-build
-              - job-workspace
-              - job-cancel
-              - run-delete
-              - run-update
-              - scm-tag
-            anonymous:
-              - job-discover
-              - job-read
-              - job-extended-read
+    .. literalinclude:: /../../tests/properties/fixtures/authorization.yaml
+       :language: yaml
     """
 
     mapping = {
+        'credentials-create':
+        'com.cloudbees.plugins.credentials.CredentialsProvider.Create',
+        'credentials-update':
+        'com.cloudbees.plugins.credentials.CredentialsProvider.Update',
+        'credentials-delete':
+        'com.cloudbees.plugins.credentials.CredentialsProvider.Delete',
+        'credentials-view':
+        'com.cloudbees.plugins.credentials.CredentialsProvider.View',
+        'credentials-manage-domains':
+        'com.cloudbees.plugins.credentials.CredentialsProvider.ManageDomains',
         'job-delete': 'hudson.model.Item.Delete',
         'job-configure': 'hudson.model.Item.Configure',
         'job-read': 'hudson.model.Item.Read',
@@ -349,7 +349,7 @@ def authorization(parser, xml_parent, data):
         'job-cancel': 'hudson.model.Item.Cancel',
         'run-delete': 'hudson.model.Run.Delete',
         'run-update': 'hudson.model.Run.Update',
-        'scm-tag': 'hudson.scm.SCM.Tag'
+        'scm-tag': 'hudson.scm.SCM.Tag',
     }
 
     if data:
