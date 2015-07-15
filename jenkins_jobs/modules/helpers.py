87b0813c747eac1c66b6ae8ac4d1ae16c561af70
Will Soula <william.soula@drillinginfo.com>
2015-07-15 15:44:08 -0500

    Add Ability to use the AWS Cloudformation Plugin
    * This plugin gives Jenkins the ability to spawn amazon
    cloudformation stacks before running the build and stopping it at
    the end
    
    * This change adds cloudformation ability to the builders and the
    publishers to create stacks and to the publishers to tear the stack
    down
    
    Change-Id: I8041382fc6f1d569c322088a3bc8812332a131e8

--- jenkins_jobs/modules/helpers.py
+++ jenkins_jobs/modules/helpers.py
@@ -170,3 +170,54 @@ def findbugs_settings(xml_parent, data):
                                       False)).lower()
     XML.SubElement(xml_parent,
                    'usePreviousBuildAsReference').text = use_previous_build
+
+
+def cloudformation_region_dict():
+    region_dict = {'us-east-1': 'US_East_Northern_Virginia',
+                   'us-west-1': 'US_WEST_Northern_California',
+                   'us-west-2': 'US_WEST_Oregon',
+                   'eu-central-1': 'EU_Frankfurt',
+                   'eu-west-1': 'EU_Ireland',
+                   'ap-southeast-1': 'Asia_Pacific_Singapore',
+                   'ap-southeast-2': 'Asia_Pacific_Sydney',
+                   'ap-northeast-1': 'Asia_Pacific_Tokyo',
+                   'sa-east-1': 'South_America_Sao_Paulo'}
+    return region_dict
+
+
+def cloudformation_init(xml_parent, data, xml_tag):
+    cloudformation = XML.SubElement(xml_parent, 'com.syncapse.jenkinsci.'
+                                                'plugins.'
+                                                'awscloudformationwrapper.' +
+                                                xml_tag)
+    return XML.SubElement(cloudformation, 'stacks')
+
+
+def cloudformation_stack(xml_parent, stack, xml_tag, stacks, region_dict):
+    if 'name' not in stack or stack['name'] == '':
+        raise MissingAttributeException('name')
+    step = XML.SubElement(stacks, 'com.syncapse.jenkinsci.plugins.'
+                                  'awscloudformationwrapper.' +
+                                  xml_tag)
+    try:
+        XML.SubElement(step, 'stackName').text = stack['name']
+        XML.SubElement(step, 'awsAccessKey').text = stack['access-key']
+        XML.SubElement(step, 'awsSecretKey').text = stack['secret-key']
+        region = stack['region']
+    except KeyError as e:
+        raise MissingAttributeException(e.args[0])
+    if region not in region_dict:
+        raise InvalidAttributeError('region', region, region_dict.keys())
+    XML.SubElement(step, 'awsRegion').text = region_dict.get(region)
+    if xml_tag == 'SimpleStackBean':
+        prefix = str(stack.get('prefix', False)).lower()
+        XML.SubElement(step, 'isPrefixSelected').text = prefix
+    else:
+        XML.SubElement(step, 'description').text = stack.get('description', '')
+        XML.SubElement(step, 'parameters').text = stack.get('parameters', '')
+        XML.SubElement(step, 'timeout').text = str(stack.get('timeout', '0'))
+        XML.SubElement(step, 'sleep').text = str(stack.get('sleep', '0'))
+        try:
+            XML.SubElement(step, 'cloudFormationRecipe').text = stack['recipe']
+        except KeyError as e:
+            raise MissingAttributeException(e.args[0])
