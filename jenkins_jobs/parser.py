cffff37391aac93323948de219b16cc49e4e4e4b
Wayne <wayne@puppetlabs.com>
2015-08-13 10:19:11 -0700

    WIP Fix defaults application.
    Prior to this change defaults were applied differently when set for job-template
    and job-group/project. This commit unifies the behavior such that both JJB
    variables and JJB expansion parameters (triggers, builders, etc) will be pulled
    in from defaults set at both places (job-template vs job-group/project).
    
    It also adds the ability to specify a list of defaults that successively merge
    on top of each other. This commit is WIP because it needs tests to validate this
    behavior.
    
    We also remove redundant applyDefaults call for non-template jobs.
    
    Change-Id: I5e42815754790bc6f62f5b5dcc7260864c9cf447

--- jenkins_jobs/parser.py
+++ jenkins_jobs/parser.py
@@ -110,30 +110,37 @@ class YamlParser(object):
             logger.warn(message)
 
     def getJob(self, name):
-        job = self.data.get('job', {}).get(name, None)
-        if not job:
-            return job
-        return self.applyDefaults(job)
+        return self.data.get('job', {}).get(name, None)
 
     def getJobGroup(self, name):
         return self.data.get('job-group', {}).get(name, None)
 
     def getJobTemplate(self, name):
-        job = self.data.get('job-template', {}).get(name, None)
-        if not job:
-            return job
-        return self.applyDefaults(job)
+        return self.data.get('job-template', {}).get(name, None)
 
     def applyDefaults(self, data, override_dict=None):
         if override_dict is None:
             override_dict = {}
 
+        def get_defaults(parser, defaults_name):
+            defaults_dict = self.data.get('defaults', {})
+            defaults = copy.deepcopy(defaults_dict.get(defaults_name, {}))
+
+            if defaults == {} and defaults_name != 'global':
+                raise JenkinsJobsException("Unknown defaults set: '{0}'"
+                                           .format(defaults_name))
+
+            return defaults
+
         whichdefaults = data.get('defaults', 'global')
-        defaults = copy.deepcopy(self.data.get('defaults',
-                                 {}).get(whichdefaults, {}))
-        if defaults == {} and whichdefaults != 'global':
-            raise JenkinsJobsException("Unknown defaults set: '{0}'"
-                                       .format(whichdefaults))
+
+        defaults = {}
+        if isinstance(whichdefaults, list):
+            for name in whichdefaults:
+                tmp_defaults = get_defaults(self, name)
+                defaults.update(tmp_defaults)
+        else:
+            defaults = get_defaults(self, whichdefaults)
 
         for key in override_dict.keys():
             if key in defaults.keys():
@@ -250,6 +257,17 @@ class YamlParser(object):
     def expandYamlForTemplateJob(self, project, template, jobs_glob=None):
         dimensions = []
         template_name = template['name']
+
+        # project or job-group defaults should always override template
+        # defaults if they exist.
+        project_defaults = project.get('defaults', ['global'])
+        template_defaults = template.get('defaults', "")
+
+        project['defaults'] = project_defaults
+
+        if len(template_defaults) == 0:
+            template['defaults'] = ['global']
+
         # reject keys that are not useful during yaml expansion
         for k in ['jobs']:
             project.pop(k)
@@ -268,6 +286,13 @@ class YamlParser(object):
 
         for values in itertools.product(*dimensions):
             params = copy.deepcopy(project)
+
+            if len(template_defaults) == 0:
+                params['defaults'] = ['global'].extend(project_defaults)
+            else:
+                params['defaults'] = [template_defaults].extend(
+                    project_defaults)
+
             params = self.applyDefaults(params, template)
 
             expanded_values = {}
