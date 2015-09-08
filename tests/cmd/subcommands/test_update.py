34185698d7651523033b237caf7f8df7d67b9210
Brian Torres <btorres@kopru.lu>
2015-09-07 21:47:50 -0700

    Add delete-unmanaged feature
    Change-Id: Ia056ad08d659b722138178b873dc9efa6ff99271

--- tests/cmd/subcommands/test_update.py
+++ tests/cmd/subcommands/test_update.py
@@ -18,6 +18,7 @@ import os
 
 from jenkins_jobs import cmd
 from jenkins_jobs import builder
+from jenkins_jobs import constants
 from tests.base import mock
 from tests.cmd.test_cmd import CmdTestsBase
 
@@ -99,3 +100,88 @@ class UpdateTests(CmdTestsBase):
                           "Called with: %s" % (2, delete_job_mock.call_count,
                                                delete_job_mock.mock_calls))
         delete_job_mock.assert_has_calls(calls, any_order=True)
+
+    @mock.patch('jenkins_jobs.builder.Jenkins.is_job', return_value=True)
+    @mock.patch('jenkins_jobs.builder.Jenkins.get_jobs')
+    @mock.patch('jenkins_jobs.builder.Builder.delete_job')
+    def test_update_jobs_and_delete_unmanaged(self, delete_job_mock, get_jobs_mock,
+                                          is_job_mock):
+        """
+        Test update behaviour with --delete-unmanaged option
+
+        Test update of jobs with the --delete-unmanaged option enabled, where
+        only some jobs result in has_changed() to limit the number of times
+        update_job is called, and have the get_jobs() method return additional
+        jobs with and without the MAGIC_MANAGE_STRING. From there,
+        delete_unmanaged should only delete the job without the
+        MAGIC_MANAGE_STRING as a way to enforce the use of JJB.
+        """
+        jobs = ['unmanaged_job001', 'unmanaged_job002']
+        extra_jobs = [{'name': name,
+                       'description': constants.MAGIC_MANAGE_STRING}
+                      for name in jobs]
+        extra_jobs = extra_jobs + [{'name': 'delete-me'}]
+        builder_obj = builder.Builder('http://jenkins.example.com',
+                                      'doesnot', 'matter',
+                                      plugins_list={})
+
+        def _get_jobs():
+            try:
+                return builder_obj.parser.jobs + extra_jobs
+            except:
+                builder_obj.load_files([])
+                return builder_obj.parser.jobs + extra_jobs
+        get_jobs_mock.side_effect = _get_jobs
+
+        def _is_managed(job_name):
+            magic_string = constants.MAGIC_MANAGE_STRING
+
+            jobs_with_job_name = [job for job in _get_jobs()
+                                  if job['name'] == job_name]
+            managed_jobs = [job for job in jobs_with_job_name
+                            if job.get('description') == magic_string]
+
+            return any(managed_jobs)
+
+        # override cache to ensure Jenkins.update_job called a limited number
+        # of times
+        self.cache_mock.return_value.has_changed.side_effect = (
+            [True] * 2 + [False] * 3)
+
+        path = os.path.join(self.fixtures_path, 'cmd-002.yaml')
+        args = self.parser.parse_args(['update', '--delete-unmanaged', path])
+
+        with mock.patch('jenkins_jobs.builder.Jenkins.update_job') \
+            as update_job_mock:
+            with mock.patch('jenkins_jobs.builder.Builder.delete_unmanaged',
+                            side_effect=builder_obj.delete_unmanaged) \
+                as delete_unmanaged_mock:
+                with mock.patch('jenkins_jobs.builder.Builder.delete_job') \
+                    as delete_job:
+                    with mock.patch('jenkins_jobs.builder.Jenkins.is_managed',
+                                    side_effect=_is_managed):
+
+                        cmd.execute(args, self.config)
+
+                    self.assertEquals(1, delete_job.call_count,
+                                      "Expected Builder.delete_job \
+                                       to be called '%d' "
+                                      "times got '%d' calls instead.\n"
+                                      "Called with: %s" %
+                                      (1, delete_job.call_count,
+                                       delete_job.mock_calls))
+
+                self.assertEquals(1, delete_unmanaged_mock.call_count,
+                                  "Expected Builder.delete_unmanaged \
+                                   to be called '%s' "
+                                  "times, got '%s' calls instead.\n"
+                                  "Called with: %s" %
+                                  (1, str(delete_unmanaged_mock.call_count),
+                                   delete_unmanaged_mock.mock_calls))
+
+            self.assertEquals(2, update_job_mock.call_count,
+                              "Expected Jenkins.update_job to be called '%d' "
+                              "times, got '%d' calls instead.\n"
+                              "Called with: %s" %
+                              (2, update_job_mock.call_count,
+                               update_job_mock.mock_calls))
