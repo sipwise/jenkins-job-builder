4164f24ad25775953db7c44b39c40e7c0df99ef5
Brian Torres <btorres@kopru.lu>
2015-09-21 00:28:25 -0700

    Use xml instead of job description for is_managed
    Inject a jjb xml attribute into the job to determine if a job is
    managed. Old method relied on modifying the description with a magic
    string which followed a job on copy. The xml attribute does not follow
    the job on copy making for a more reliable determination of management.
    
    XML tree follows the following format:
    <jjb>
        <managed>true</managed>
    </jjb>
    
    story: 2000357
    Change-Id: I318a5901052fe54664c8578b51c07a1aa01b8586

--- jenkins_jobs/builder.py
+++ jenkins_jobs/builder.py
@@ -178,6 +178,16 @@ class Jenkins(object):
         return self.jobs
 
     def is_managed(self, job_name):
+        xml = XML.fromstring(self.jenkins.get_job_config(job_name))
+        try:
+            jjb_attr = xml.find('jjb', None)
+            jjb_managed = jjb_attr.find('managed')
+            return jjb_managed.text == 'true'
+        except (TypeError, AttributeError):
+            pass
+        return False
+
+    def is_legacy_managed(self, job_name):
         xml = self.jenkins.get_job_config(job_name)
         try:
             out = XML.fromstring(xml)
@@ -257,12 +267,21 @@ class Builder(object):
         if keep is None:
             keep = [job.name for job in self.parser.xml_jobs]
         for job in jobs:
-            if job['name'] not in keep and \
-                    self.jenkins.is_managed(job['name']):
-                logger.info("Removing obsolete jenkins job {0}"
-                            .format(job['name']))
-                self.delete_job(job['name'])
-                deleted_jobs += 1
+            if job['name'] not in keep:
+                if self.jenkins.is_legacy_managed(job['name']) \
+                    and not self.jenkins.is_managed(job['name']):
+
+                    logger.warning("{0} is using a legacy format and will be"
+                                   "skipped. Run update without delete-old"
+                                   "to update before trying delete-old again."
+                                   .format(job['name']))
+                    continue
+                if self.jenkins.is_managed(job['name']):
+                    logger.info("Removing obsolete jenkins job {0}"
+                                .format(job['name']))
+
+                    self.delete_job(job['name'])
+                    deleted_jobs += 1
             else:
                 logger.debug("Ignoring unmanaged jenkins job %s",
                              job['name'])
