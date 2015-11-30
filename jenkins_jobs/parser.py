485843e653cad8cb5b4ff4763e8970ab52a12aa8
Darragh Bailey <dbailey@hp.com>
2015-11-30 13:20:15 +0100

    Basic folder support
    Allows specifying a folder attribute for each job generated, which in
    turn is used when creating or uploading to place the job under the
    requested folder.
    
    The job name is expanded after defaults are applied, to support the
    attribute being defined within a set of defaults applied to a number of
    jobs.
    
    This in turn allows for multiple jobs with the same basename to exist,
    provided they are targeted at different folders.
    
    Does not support creating the folders if they do not exist.
    
    Change-Id: I8c2157c4c81087cc972a048d1b88d5f08ac65361

--- jenkins_jobs/parser.py
+++ jenkins_jobs/parser.py
@@ -218,6 +218,12 @@ class YamlParser(object):
             job["description"] = description + \
                 self._get_managed_string().lstrip()
 
+    def _getfullname(self, data):
+        if 'folder' in data:
+            return "%s/%s" % (data['folder'], data['name'])
+
+        return data['name']
+
     def expandYaml(self, registry, jobs_glob=None):
         changed = True
         while changed:
@@ -228,15 +234,17 @@ class YamlParser(object):
                         changed = True
 
         for job in self.data.get('job', {}).values():
+            job = self._applyDefaults(job)
+            job['name'] = self._getfullname(job)
             if jobs_glob and not matches(job['name'], jobs_glob):
                 logger.debug("Ignoring job {0}".format(job['name']))
                 continue
             logger.debug("Expanding job '{0}'".format(job['name']))
-            job = self._applyDefaults(job)
             self._formatDescription(job)
             self.jobs.append(job)
 
         for view in self.data.get('view', {}).values():
+            view['name'] = self._getfullname(view)
             logger.debug("Expanding view '{0}'".format(view['name']))
             self._formatDescription(view)
             self.views.append(view)
@@ -377,6 +385,7 @@ class YamlParser(object):
             expanded = deep_format(
                 template, params,
                 self.jjb_config.yamlparser['allow_empty_variables'])
+            expanded['name'] = self._getfullname(expanded)
 
             job_name = expanded.get('name')
             if jobs_glob and not matches(job_name, jobs_glob):
