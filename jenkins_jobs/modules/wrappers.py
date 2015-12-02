3790bfe0ee5bb03c237d2ab917df0a73ff0aaf76
Darragh Bailey <dbailey@hpe.com>
2015-12-02 11:12:24 +0000

    Infer and build entry points from component modules
    Use the component type entry points to infer modules to scan
    corresponding modules and automatically construct entry points for any
    functions associated with yaml entries in JJB definitions.
    
    This removes the need for new JJB modules added directly to the project
    to also need to be listed in the setup.cfg to be reachable. Simply
    adding the needed functionality to the correct file ensures that it is
    available.
    
    Change-Id: I2a0fd8f6720ddfd8582b58e47556a87ed1ad553d

--- jenkins_jobs/modules/wrappers.py
+++ jenkins_jobs/modules/wrappers.py
@@ -1653,7 +1653,7 @@ def android_emulator(parser, xml_parent, data):
 
 
 def artifactory_maven(parser, xml_parent, data):
-    """ yaml: artifactory-maven
+    """yaml: artifactory-maven
     Wrapper for non-Maven projects. Requires the
     :jenkins-wiki:`Artifactory Plugin <Artifactory+Plugin>`
 
@@ -1698,7 +1698,7 @@ def artifactory_maven(parser, xml_parent, data):
 
 
 def artifactory_generic(parser, xml_parent, data):
-    """ yaml: artifactory-generic
+    """yaml: artifactory-generic
     Wrapper for non-Maven projects. Requires the
     :jenkins-wiki:`Artifactory Plugin <Artifactory+Plugin>`
 
@@ -1775,7 +1775,7 @@ def artifactory_generic(parser, xml_parent, data):
 
 
 def artifactory_maven_freestyle(parser, xml_parent, data):
-    """ yaml: artifactory-maven-freestyle
+    """yaml: artifactory-maven-freestyle
     Wrapper for Free Stype projects. Requires the Artifactory plugin.
     Requires :jenkins-wiki:`Artifactory Plugin <Artifactory+Plugin>`
 
