d4fd6b9a1ec55d86f6a6d09ecacc489e92402efb
Kien Ha <kienha9922@gmail.com>
2015-12-25 20:14:25 -0500

    Add support for testfairy plugin
    Change-Id: I09226dc3bee88b4c1ecb6649ff7b971fce2541c7
    Signed-off-by: Kien Ha <kienha9922@gmail.com>

--- tests/modules/test_helpers.py
+++ tests/modules/test_helpers.py
@@ -13,17 +13,14 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import os
 import testtools
 from testtools.matchers import Equals
 import xml.etree.ElementTree as XML
 import yaml
 
-import jenkins_jobs
 from jenkins_jobs.errors import MissingAttributeError
 from jenkins_jobs.modules.helpers import convert_mapping_to_xml_fail_required
 from tests.base import LoggingFixture
-from tests.base import mock
 
 
 class TestCaseTestHelpers(LoggingFixture, testtools.TestCase):
@@ -33,20 +30,29 @@ class TestCaseTestHelpers(LoggingFixture, testtools.TestCase):
         Tests the test_convert_mapping_to_xml_fail_required function
         """
 
-        # Test Default values
+        # Test default values
         default_data = yaml.load("string: hello")
         default_mappings = [('default-string', 'defaultString', 'default')]
         default_root = XML.Element('testdefault')
-
-        convert_mapping_to_xml_fail_required(default_data, default_root, default_mappings)
+        convert_mapping_to_xml_fail_required(default_data, default_root,
+                                             default_mappings)
         result = default_root.find('defaultString').text
-
         self.assertThat(result, Equals('default'))
 
-        # Test user Input
+        # Test user input
+        user_input_data = yaml.load("user-input-string: hello")
+        user_input_mappings = [('user-input-string', 'userInputString',
+                                'user-input')]
+        user_input_root = XML.Element('testUserInput')
+        convert_mapping_to_xml_fail_required(user_input_data, user_input_root,
+                                             user_input_mappings)
+        result = user_input_root.find('userInputString').text
+        self.assertThat(result, Equals('hello'))
 
-        # Test missing Required input
+        # Test missing required input
         required_data = yaml.load("string: hello")
         required_mappings = [('required-string', 'requiredString', None)]
         required_root = XML.Element('testrequired')
-        self.assertRaises(MissingAttributeError, convert_mapping_to_xml_fail_required, required_data, required_root, required_mappings)
+        self.assertRaises(MissingAttributeError,
+                          convert_mapping_to_xml_fail_required,
+                          required_data, required_root, required_mappings)
