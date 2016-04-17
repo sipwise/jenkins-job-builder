109d1a53df86b886e9c0a40388dee622a60a94d0
Christian Rodemeyer <christian@atombrenner.de>
2016-04-17 15:47:26 +0200

    add update-build-name builder
    Change-Id: Ia0b11bdae7251ff48f3ef21e40e5d09c76195ba7

--- jenkins_jobs/modules/builders.py
+++ jenkins_jobs/modules/builders.py
@@ -1820,6 +1820,33 @@ def sbt(parser, xml_parent, data):
     XML.SubElement(sbt, 'subdirPath').text = data.get(
         'subdir-path', '')
 
+def update_build_name(parser, xml_parent, data):
+    """yaml: update-build-name
+    Updates the build name as build step.
+    Requires the Jenkins :jenkins-wiki:`Build Name Setter Plugin
+    <Build+Name+Setter+Plugin>`.
+
+    :arg str file: update build name with the content of this file
+    :arg str macro: update build name with the result of this macro
+    :arg bool macroFirst: if both file and macro are defined,
+        use macro first (default false)
+
+    Example:
+
+    .. literalinclude:: ../../tests/builders/fixtures/update_build_name.yaml
+       :language: yaml
+    """
+    node = XML.SubElement(xml_parent, 'org.jenkinsci.plugins.buildnameupdater.'
+                                      'BuildNameUpdater')
+    file = data.get('file', '')
+    macro = data.get('macro', '')
+    macroFirst = data.get('macroFirst', False)
+    XML.SubElement(node, 'buildName').text = file
+    XML.SubElement(node, 'macroTemplate').text = macro
+    XML.SubElement(node, 'fromFile').text = 'true' if file else 'false'
+    XML.SubElement(node, 'fromMacro').text = 'true' if macro else 'false'
+    XML.SubElement(node, 'macroFirst').text = str(macroFirst).lower()
+
 
 def critical_block_start(parser, xml_parent, data):
     """yaml: critical-block-start
