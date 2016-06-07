4782f04c0fc3f840e83b716f47db6c8ab17cfacd
Thanh Ha <thanh.ha@linuxfoundation.org>
2016-06-07 16:10:53 -0400

    Allow JJB to spoof plugin_info via yaml
    Getting plugin info requires Administrator permissions. It seems the
    test command allows spoofing the plugin_info details via a yaml
    configuration file. This patch allows the same command to be passed to
    the update command to allow us to configure the plugin versions manually
    and not require administrator permissions.
    
    Additionally this patch adds a new command called generate-plugins-info
    which can generate the plugins_info.yaml file to pass to the
    plugin_info_path.
    
    See:
    http://lists.openstack.org/pipermail/openstack-infra/2016-June/004388.html
    
    Change-Id: I5a34979407d863a84f34afbf8f565081ec52190a
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jenkins_jobs/cmd.py
+++ jenkins_jobs/cmd.py
@@ -111,6 +111,11 @@ def create_parser():
     parser_update.add_argument('--delete-old', help='delete obsolete jobs',
                                action='store_true',
                                dest='delete_old', default=False,)
+    parser_update.add_argument('-p', '--plugin-info', dest='plugins_info_path',
+                               default=None,
+                               help='path to plugin info YAML file. Can be '
+                               'used to spoof plugin_info when administrator '
+                               'permissions isn\'t available to JJB.')
     parser_update.add_argument('--workers', dest='n_workers', type=int,
                                default=1, help='number of workers to use, 0 '
                                'for autodetection and 1 for just one worker.')
@@ -120,7 +125,8 @@ def create_parser():
     parser_test.add_argument('path', help='colon-separated list of paths to'
                                           ' YAML files or directories',
                              nargs='?', default=sys.stdin)
-    parser_test.add_argument('-p', dest='plugins_info_path', default=None,
+    parser_test.add_argument('-p', '--plugin-info', dest='plugins_info_path',
+                             default=None,
                              help='path to plugin info YAML file')
     parser_test.add_argument('-o', dest='output_dir', default=sys.stdout,
                              help='path to output XML')
@@ -166,6 +172,12 @@ def create_parser():
         help='Password or API token to use for authenticating towards '
         'Jenkins. This overrides the password specified in the '
         'configuration file.')
+
+    # subparser: generate-plugins-info
+    parser_update = subparser.add_parser(
+        'generate-plugins-info',
+        help='Generate plugins_info.yaml by querying Jenkins server.',
+        parents=[recursive_parser])
 
     return parser
 
@@ -378,6 +390,20 @@ def execute(options, config):
         builder.update_jobs(options.path, options.name,
                             output=options.output_dir,
                             n_workers=1)
+
+    elif options.command == 'generate-plugins-info':
+        plugin_data = builder.jenkins.get_plugins_info()
+        plugins_info = []
+        for plugin in plugin_data:
+            info = {
+                'longName': plugin['longName'],
+                'shortName': plugin['shortName'],
+                'version': str(plugin['version']),
+            }
+            plugins_info.append(info)
+        with open('plugins_info.yaml', 'w') as outfile:
+            outfile.write(yaml.dump(plugins_info))
+        logger.info("Generated plugins_info.yaml file")
 
 
 def version():
