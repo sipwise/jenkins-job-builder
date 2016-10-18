cd8339250bf80d794532717be60babbca288d67e
Thomas Bechtold <tbechtold@suse.com>
2016-10-18 07:56:57 +0200

    Fix timeout wrapper version detection
    If, for whatever reason, the version of the build-timeout
    plugin can not be detected, assume that we use a newer version (>= 1.14).
    Version 1.14 is already ~ 2 1/2 years old so this is a sane assumption.
    Also adjust fixtures to use the newer plugin version xml output.
    
    Change-Id: I86295933d0c3247f55d0114473fc9c8f8feb7441
    Signed-off-by: Bertrand Roussel <broussel@sierrawireless.com>

--- jenkins_jobs/modules/wrappers.py
+++ jenkins_jobs/modules/wrappers.py
@@ -313,13 +313,18 @@ def timeout(registry, xml_parent, data):
     twrapper = XML.SubElement(xml_parent, prefix + 'BuildTimeoutWrapper')
 
     plugin_info = registry.get_plugin_info(
-        "Jenkins build timeout plugin")
-    version = pkg_resources.parse_version(plugin_info.get("version", "0"))
+        "Build Timeout")
+    if not "version" in plugin_info:
+        plugin_info = registry.get_plugin_info(
+            "Jenkins build timeout plugin")
+    version = pkg_resources.parse_version(plugin_info.get("version", None))
 
     valid_strategies = ['absolute', 'no-activity', 'likely-stuck', 'elastic',
                         'deadline']
 
-    if version >= pkg_resources.parse_version("1.14"):
+    # NOTE(toabctl): if we don't know the version assume that we
+    # use a newer version of the plugin
+    if not version or version >= pkg_resources.parse_version("1.14"):
         strategy = data.get('type', 'absolute')
         if strategy not in valid_strategies:
             InvalidAttributeError('type', strategy, valid_strategies)
