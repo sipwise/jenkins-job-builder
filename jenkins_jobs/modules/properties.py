1f0addb0cf706e380d2ef9589a09d9a876c468a5
akulakhan <kuhlmann.andrew@gmail.com>
2016-11-29 14:44:31 -0800

    Add matching parameter to throttle concurrent jobs
    Change-Id: I6d211508d774df2312942d56a840bf311bb455d4
    Co-Authored-By: Tan Heng Yeow <E0032242@u.nus.edu>
    Signed-off-by: Tan Heng Yeow <E0032242@u.nus.edu>

--- jenkins_jobs/modules/properties.py
+++ jenkins_jobs/modules/properties.py
@@ -241,6 +241,10 @@ def throttle(registry, xml_parent, data):
     :arg list categories: multiproject throttle categories
     :arg bool matrix-builds: throttle matrix master builds (default true)
     :arg bool matrix-configs: throttle matrix config builds (default false)
+    :arg str parameters-limit: prevent jobs with matching parameters from
+         running concurrently. (default false)
+    :arg list parameters-check-list: Comma-separated list of parameters
+        to use when comparing jobs. (optional)
 
     Example:
 
@@ -279,6 +283,16 @@ def throttle(registry, xml_parent, data):
     ]
     helpers.convert_mapping_to_xml(
         matrixopt, data, mapping, fail_required=True)
+
+    mapping = [
+        ('parameters-limit', 'limitOneJobWithMatchingParams', False),
+    ]
+    helpers.convert_mapping_to_xml(
+        throttle, data, mapping, fail_required=False)
+
+    params_to_use = data.get('parameters-check-list', [])
+    XML.SubElement(throttle, 'paramsToUseForLimit').text = ",".join(
+        params_to_use)
 
 
 def branch_api(registry, xml_parent, data):
