44c115c3387c0556f3d2a890b3fb11d005c3532a
Sorin Sbarnea <ssbarnea@redhat.com>
2017-05-10 12:53:58 +0100

    list jobs using alphanum sorting
    This improves UX by using a alphanum sorting which
    is more natural to humans.
    
    Change-Id: Ice9c3de282f08e85c8f8a34a27d1b6c0502ca9cb

--- jenkins_jobs/builder.py
+++ jenkins_jobs/builder.py
@@ -41,6 +41,80 @@ logger = logging.getLogger(__name__)
 
 _DEFAULT_TIMEOUT = object()
 
+# alphanum sorting utils start
+re_chunk = re.compile("([\D]+|[\d]+)")
+re_letters = re.compile("\D+")
+re_numbers = re.compile("\d+")
+
+
+def getchunk(item):
+    itemchunk = re_chunk.match(item)
+
+    # Subtract the matched portion from the original string
+    # if there was a match, otherwise set it to ""
+    item = (item[itemchunk.end():] if itemchunk else "")
+    # Don't return the match object, just the text
+    itemchunk = (itemchunk.group() if itemchunk else "")
+
+    return (itemchunk, item)
+
+
+def cmp(a, b):
+    return (a > b) - (a < b)
+
+
+def alphanum(a, b):
+    # allows sorting of many kinds of objects, using name attribe if possible
+    a = a.name if hasattr(a, 'name') else str(a)
+    b = b.name if hasattr(b, 'name') else str(b)
+
+    n = 0
+
+    while (n == 0):
+        # Get a chunk and the original string with the chunk subtracted
+        (ac, a) = getchunk(a)
+        (bc, b) = getchunk(b)
+
+        # Both items contain only letters
+        if (re_letters.match(ac) and re_letters.match(bc)):
+            n = cmp(ac, bc)
+        else:
+            # Both items contain only numbers
+            if (re_numbers.match(ac) and re_numbers.match(bc)):
+                n = cmp(int(ac), int(bc))
+            # item has letters and one item has numbers, or one item is empty
+            else:
+                n = cmp(ac, bc)
+                # Prevent deadlocks
+                if (n == 0):
+                    n = 1
+    return n
+
+
+class AlphanumSort(object):
+    def __init__(self, obj, *args):
+        self.obj = obj
+
+    def __lt__(self, other):
+        return alphanum(self.obj, other.obj) < 0
+
+    def __gt__(self, other):
+        return alphanum(self.obj, other.obj) > 0
+
+    def __eq__(self, other):
+        return alphanum(self.obj, other.obj) == 0
+
+    def __le__(self, other):
+        return alphanum(self.obj, other.obj) <= 0
+
+    def __ge__(self, other):
+        return alphanum(self.obj, other.obj) >= 0
+
+    def __ne__(self, other):
+        return alphanum(self.obj, other.obj) != 0
+
+# alphanum sorting utils end
+
 
 class JenkinsManager(object):
 
@@ -247,6 +321,9 @@ class JenkinsManager(object):
 
         if not jobs:
             return [], 0
+
+        # sorting job names using natural sorting for improving UX
+        jobs = sorted(jobs, key=AlphanumSort)
 
         # Update the jobs
         logging.debug('Updating jobs')
