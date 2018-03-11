81fdd5845caea67fa835775c46bc94295532cf74
Anil Belur <askb23@gmail.com>
2018-03-11 13:37:02 +1000

    Add support for scmtype job filters
    Add support for scmtype job filters for views.
    
    Change-Id: Ie63f36ff61258c0a467eab8dbae93b4d5ef60837
    Signed-off-by: Anil Belur <askb23@gmail.com>

--- jenkins_jobs/modules/view_list.py
+++ jenkins_jobs/modules/view_list.py
@@ -76,6 +76,12 @@ to the :ref:`view_list` definition.
                              * **stable** ('bool'): Jobs with status
                                stable. (default False)
 
+        * **scm** (`dict`)
+          :scm: * **match-type** ('str'): Jobs that match a filter to include.
+                  (default includeMatched)
+                  * **scm-type** ('str'): Type of SCM.
+                    (default hudson.scm.NullSCM)
+
     * **columns** (`list`): List of columns to be shown in view.
     * **regex** (`str`): . Regular expression for selecting jobs
       (optional)
@@ -211,6 +217,19 @@ class List(jenkins_jobs.modules.base.Base):
                     ('stable', 'stable', False),
                 ]
                 convert_mapping_to_xml(js_xml, js_data, mapping,
+                                       fail_required=True)
+
+            if jobfilter == 'scm':
+                st_xml = XML.SubElement(job_filter_xml,
+                                        'hudson.views.ScmTypeFilter')
+                st_xml.set('plugin', 'view-job-filters')
+                st_data = jobfilters.get('scm')
+                mapping = [
+                    ('match-type', 'includeExcludeTypeString',
+                        'includeMatched'),
+                    ('scm-type', 'scmType', 'hudson.scm.NullSCM'),
+                ]
+                convert_mapping_to_xml(st_xml, st_data, mapping,
                                        fail_required=True)
 
         c_xml = XML.SubElement(root, 'columns')
