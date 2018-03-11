39acc2e26b929a016f266c08167bb863b7e6ad68
Anil Belur <askb23@gmail.com>
2018-03-11 16:16:11 +1000

    Add support for user relevance job filter
    Add support for user relevance job filter for views.
    
    Change-Id: If20378e886c50f799f91e44d02d7d4e2edc9401c
    Signed-off-by: Anil Belur <askb23@gmail.com>

--- jenkins_jobs/modules/view_list.py
+++ jenkins_jobs/modules/view_list.py
@@ -76,6 +76,32 @@ to the :ref:`view_list` definition.
                              * **stable** ('bool'): Jobs with status
                                stable. (default False)
 
+        * **user-relevence** (`dict`)
+          :user-relevence: * **match-type** ('str'): Jobs that match a filter
+                             to include. (default includeMatched)
+                             * **build-count** ('str'): Count of builds.
+                               (default AtLeastOne)
+                             * **amount-type**: ('str'): Duration in hours,
+                               days or builds. (default Hours)
+                             * **amount**: ('int'): How far back to check.
+                               (default 0)
+                             * **match-user-id** ('bool'): Jobs matching
+                               user-id. (default False)
+                             * **match-user-fullname** ('bool'): Jobs
+                               matching user fullname. (default False)
+                             * **ignore-case** ('bool'): Ignore case.
+                               (default False)
+                             * **ignore-whitespace** ('bool'): Ignore
+                               whitespace. (default False)
+                             * **ignore-non-alphaNumeric** ('bool'): Ignore
+                               non-alphaNumeric. (default False)
+                             * **match-builder** ('bool'): Jobs matching
+                               builder. (default False)
+                             * **match-email** ('bool'): Jobs matching
+                               email. (default False)
+                             * **match-scm-changes** ('bool'): Jobs matching
+                               scm changes. (default False)
+
     * **columns** (`list`): List of columns to be shown in view.
     * **regex** (`str`): . Regular expression for selecting jobs
       (optional)
@@ -211,6 +237,30 @@ class List(jenkins_jobs.modules.base.Base):
                     ('stable', 'stable', False),
                 ]
                 convert_mapping_to_xml(js_xml, js_data, mapping,
+                                       fail_required=True)
+
+            if jobfilter == 'user-relevence':
+                ur_xml = XML.SubElement(job_filter_xml,
+                                        'hudson.views.UserRelevanceFilter')
+                ur_xml.set('plugin', 'view-job-filters')
+                ur_data = jobfilters.get('user-relevence')
+                mapping = [
+                    ('match-type', 'includeExcludeTypeString',
+                        'includeMatched'),
+                    ('build-count', 'buildCountTypeString', 'AtLeastOne'),
+                    ('amount-type', 'amountTypeString', 'Hours'),
+                    ('amount', 'amount', '0'),
+                    ('match-user-id', 'matchUserId', False),
+                    ('match-user-fullname', 'matchUserFullName', False),
+                    ('ignore-case', 'ignoreCase', False),
+                    ('ignore-whitespace', 'ignoreWhitespace', False),
+                    ('ignore-non-alphaNumeric', 'ignoreNonAlphaNumeric',
+                        False),
+                    ('match-builder', 'matchBuilder', False),
+                    ('match-email', 'matchEmail', False),
+                    ('match-scm-changes', 'matchScmChanges', False),
+                ]
+                convert_mapping_to_xml(ur_xml, ur_data, mapping,
                                        fail_required=True)
 
         c_xml = XML.SubElement(root, 'columns')
