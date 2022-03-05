dc24d64a024a241920c947bceb2a5b20383060c4
trunov_ms <trunov.ms@gmail.com>
2022-03-05 12:48:37 +0500

    STORY: 2009856 added support for Matrix Authorization Stategy 3.0+ version
    Change-Id: I46521033d9fd6f7f4ae59d8784f9fbfdd49958ed

--- jenkins_jobs/modules/properties.py
+++ jenkins_jobs/modules/properties.py
@@ -533,8 +533,11 @@ def authorization(registry, xml_parent, data, job_data):
 
     .. _authorization:
 
-    :arg list <name>: `<name>` is the name of the group or user, containing
-        the list of rights to grant.
+    :arg list prefix:<name>: 
+            * `prefix`  
+                * **GROUP**
+                * **USER**
+            * `<name>` is the name of the group or user, containing the list of rights to grant.
 
        :<name> rights:
             * **credentials-create**
@@ -610,7 +613,10 @@ def authorization(registry, xml_parent, data, job_data):
             for perm in perms:
                 pe = XML.SubElement(matrix, "permission")
                 try:
-                    pe.text = "{0}:{1}".format(mapping[perm], username)
+                    if username.upper().startswith('GROUP:') or username.upper().startswith('USER:'):
+                        pe.text = "{0}:{1}:{2}".format(username.split(':')[0].upper(), mapping[perm], username.split(':')[1])
+                    else:
+                        pe.text = "{0}:{1}".format(mapping[perm], username)
                 except KeyError:
                     raise InvalidAttributeError(username, perm, mapping.keys())
 
